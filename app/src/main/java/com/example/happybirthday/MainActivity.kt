dac81bdcb59b8f4d77502611996854c4083621a8
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2022-12-25 18:22:28 +0100

    add androidparty image

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -10,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
@@ -24,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText("Happy Birthday, Chiara!", "Vik")
+                    BirthdayGreetingWithImage("Happy Birthday, Chiara!", "Vik")
                 }
             }
         }
@@ -45,13 +48,25 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        BirthdayGreetingWithText(message, from)
+    }
+}
+
 @Preview(
     name = "My Preview",
-    showBackground = true,
-    showSystemUi = true)
+    showBackground = false,
+    showSystemUi = false)
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("Happy Birthday, Chiara!", "Vik")
+        BirthdayGreetingWithImage("Happy Birthday, Chiara!", "Vik")
     }
 }
