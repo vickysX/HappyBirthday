b1caed397bbde07a2d8e5025195e51bc8d2c4aac
Vittorio Sassu <100283240+vickysX@users.noreply.github.com>
2022-12-25 17:58:01 +0100

    add and lay text out in a column

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BirthdayGreetingWithText("Happy Birthday, Chiara!", "Vik")
                 }
             }
         }
@@ -30,14 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+        Text(
+            text = "- from $from",
+            fontSize = 24.sp
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "My Preview",
+    showBackground = true,
+    showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        BirthdayGreetingWithText("Happy Birthday, Chiara!", "Vik")
     }
 }
